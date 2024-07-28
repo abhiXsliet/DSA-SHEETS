478d4de000e3913c2100212558a7653e19ac5c7c
Abhishek Kumar <abhixsliet@gmail.com>
2024-07-28 07:28:52 +0530

    Section : Arrays - I Modified

--- SDE Sheet Striver/Arrays-i/004-maximum-subarray.cpp
+++ SDE Sheet Striver/Arrays-i/004-maximum-subarray.cpp
@@ -25,22 +25,42 @@ private:
         return max( left_sum + right_sum - nums[mid], max(left_sum, right_sum) );
     }
 
-    // TC = O(N * log(N))
-    // SC = O(N) -> recursive depth
-    int maxSumSubArray(vector<int>& nums, int l, int h) {
-        if(l > h) return INT_MIN;
-        if(l == h) return nums[l];
-
-        int mid = l + (h - l)/2;
-        return max( max(maxSumSubArray(nums, l, mid-1), 
-                        maxSumSubArray(nums, mid+1, h) ), 
-                        maxSumCenter(nums, l, mid,  h) );
+    // TC : O(N^3)
+    // SC : O(1)
+    int brute(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        for (int i = 0; i < n; i ++) {
+            for (int j = i; j < n; j ++) {
+                int sum = 0;
+                for (int k = i; k <= j; k ++) {
+                    sum += nums[k];
+                }
+                maxSum = max(maxSum, sum);
+            }
+        }
+        return maxSum;
     }
 
-    // TC = O(N)
-    // SC = O(1)
-    int kadanes_max_sum_subarray(vector<int>& nums) {
-        int sum = 0;
+    // TC : O(N^2)
+    // SC : O(1)
+    int better(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        for (int i = 0; i < n; i ++) {
+            int sum = 0;
+            for (int j = i; j < n; j ++) {
+                sum += nums[j];
+                maxSum = max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+
+    // TC : O(N)
+    // SC : O(1)
+    int optimal(vector<int>& nums) {
+        int sum  = 0;
         int maxi = INT_MIN;
         for(int i = 0; i < nums.size(); i++) {
             sum += nums[i];
@@ -49,10 +69,49 @@ private:
         }
         return maxi; 
     }   
+
+    // TC : O(N)
+    // SC : O(1)
+    int followUp(vector<int>& nums) {
+        int sum    = 0;
+        int maxi   = INT_MIN;
+        int ansS   = -1;
+        int ansE   = -1;
+        int start  = -1;
+        for(int i = 0; i < nums.size(); i++) {
+            if (sum == 0) start = i;
+            sum += nums[i];
+            if(sum > maxi) {
+                maxi = max(maxi, sum);
+                ansS = start;
+                ansE = i;
+            }
+            if(sum < 0) sum = 0;
+        }
+        for (int i = ansS; i <= ansE; i ++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+        return maxi; 
+    }   
+
+    // TC : O(N * log(N))
+    // SC : O(N) -> recursive depth
+    int rec(vector<int>& nums, int l, int h) {
+        if(l > h) return INT_MIN;
+        if(l == h) return nums[l];
+
+        int mid = l + (h - l)/2;
+        return max( max(rec(nums, l, mid - 1), 
+                        rec(nums, mid + 1, h) ), 
+                        maxSumCenter(nums, l, mid,  h) );
+    }
 public:
     int maxSubArray(vector<int>& nums) {
-        // return maxSumSubArray(nums, 0, nums.size() - 1);
-
-        return kadanes_max_sum_subarray(nums);
+        // return brute(nums);
+        // return better(nums);
+        // return optimal(nums);   // KADANE's
+        return followUp(nums);  // Follow Up : print the maxSum Subarray
+        // return rec(nums, 0, nums.size() - 1); // Recursion
     }
 };
